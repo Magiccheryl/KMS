16a1436ac4a16fe205bf76a30d49c85f8a0d8441
ansonqian19 <chengqian19@gmail.com>
2021-05-29 10:32:36 -0400

    firebase config

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyBAnrLs8YDQiEcUuMu-SuVwYpI5A2QKLbc",
+  authDomain: "kiei451-final-project.firebaseapp.com",
+  projectId: "kiei451-final-project",
+  storageBucket: "kiei451-final-project.appspot.com",
+  messagingSenderId: "1064640452521",
+  appId: "1:1064640452521:web:8257c6738bf78c693d16b8"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
