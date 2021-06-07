3e1cd1fa66a6c868181d82446ca9fd53754fe132
Sienna Kim <sienna.kim@kellogg.northwestern.edu>
2021-06-07 17:51:11 -0500

    index complete

--- course.js
+++ course.js
@@ -39,17 +39,6 @@ firebase.auth().onAuthStateChanged(async function(user) {
         })
         
         
-        // Build the URL for our material API
-        let url = `/.netlify/functions/posts`
-      
-        // Fetch the url, wait for a response, store the response in memory
-        let response = await fetch(url)
-      
-        // Ask for the json-formatted data from the response, wait for the data, store it in memory
-        let json = await response.json()
-      
-        // Write the json-formatted data to the console in Chrome
-        console.log(json)
         // Grab a reference to the element with class name "material" in memory
         
 
--- netlify/functions/posts.js
+++ netlify/functions/posts.js
@@ -14,7 +14,7 @@ exports.handler = async function(event) {
   let db = firebase.firestore()
 
   // perform a query against firestore for all posts, wait for it to return, store in memory
-  let postsQuery = await db.collection(`courses`).orderBy(`created`, `desc`).get()
+  let postsQuery = await db.collection(`courses`).get()
 
   // retrieve the documents from the query
   let posts = postsQuery.docs
@@ -27,38 +27,63 @@ exports.handler = async function(event) {
     // get the data from the document
     let postData = posts[postIndex].data()
 
-    // perform a query to get the number of likes for this post
-    let materialQuery = await db.collection(`materials`).where(`courseId`, `==`, postId).get()
-
-    // the number of likes is the number of documents returned
-    let materialSummary = materialQuery.summary
-    let materialAttachment = materialQuery.attachment
-
+    
     // create an Object to be added to the return value of our lambda
     let courseObject = {
       id: postId,
       code: postData.code,
       name: postData.name,
       quarter: postData.quarter,
-      summary: materialSummary,
       attachment: materialAttachment,
+      materials: [],
       takeaways: []      
     }
+
+    // perform a query to get the number of likes for this post
+    let materialQuery = await db.collection(`materials`).where(`courseId`, `==`, postId).get()
+
+    // the number of likes is the number of documents returned
+    let materialInput = materialQuery.docs
+
+    for (let materialIndex=0; materialIndex < materialInput.length; materialIndex++) {
+      //get the data from the materials document
+
+      let materialData = materialInput[materialIndex].data()
+
+      // create an objett to be added to the materials array of the post
+      let materialsObject = {
+        summary: materialData.summary,
+        attachment: materialData.attachment
+
+      }
+
+      //add the Object to the post
+      courseObject.materials.push(materialsObject)
+
+    }
+
+    //add the Object tothe retrun value
+    returnValue.push(courseObject)
+
+    
+    let materialAttachment = materialQuery.attachment
+
+
     let takeawaysQuery = await db.collection(`takeaways`).where(`courseId`, `==`, postId).get()
 
     let takeaways = takeawaysQuery.docs
 
     for (let takeawaysIndex=0; takeawaysIndex < takeaways.length; takeawaysIndex++) {
-      // get the id from the comment document
+      // get the id from the takeaway document
       let takeawayId = takeaways[takeawaysIndex].userId
 
-      // get the data from the comment document
+      // get the data from the takeaway document
       let takeawayData = takeaways[takeawaysIndex].data()
 
-      // create an Object to be added to the comments Array of the post
+      // create an Object to be added to the takeaway Array of the post
       let takeawaysObject = {
         id: takeawayId,
-        body: takeawaytData.body,
+        body: takeawaytData.body
       
       }
 
