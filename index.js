92024b320a78f5cb8ff6c4eacf7fdbf56348b68e
Sienna Kim <sienna.kim@kellogg.northwestern.edu>
2021-06-06 23:11:50 -0500

    index v2

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 // back end for Start Page and Logged-in Homepage
-
+//standard event listener for Firebase auth
 firebase.auth().onAuthStateChanged(async function(user) {
+  
   if (user) {
     // Signed in
-    console.log('signed in')
+    console.log(user)
+    // Build the markup for the sign-out button and set the HTML in the header
+    document.querySelector(`.sign-in-or-sign-out`).innerHTML = `<button class ="text-pink-500 underline sign-out"> Sign Out </button>`
+    
+    //get a reference to the sign out button
+    let signOutButton = document. querySelector ('.sign-out')
+    
+    //handle the sign out button click
+    signOutButton.addEventListener(`click`, function(event) {
+      //sign out of firebase authentication
+      firebase.auth().signOut()
+      //redirect to the home page
+      document.location.href = `index.html`
+       // Signed out
+      console.log('signed out')
+    })
   } else {
-    // Signed out
-    console.log('signed out')
-
+    //user is not logged-in so show login
+    console.log(`Not logged in!`)
     // Initializes FirebaseUI Auth
     let ui = new firebaseui.auth.AuthUI(firebase.auth())
-
     // FirebaseUI configuration
     let authUIConfig = {
       signInOptions: [
@@ -18,8 +32,8 @@ firebase.auth().onAuthStateChanged(async function(user) {
       ],
       signInSuccessUrl: 'homepage.html'
     }
-
     // Starts FirebaseUI Auth
     ui.start('.sign-in-or-sign-out', authUIConfig)
   }
 })
+
